5ba0c8cd3e62988ee1cb8097ce4001e42bc1a9e4
pdterron <pdterron@cibertec.edu.pe>
2023-09-25 18:35:49 -0500

    Se cambio principa

--- src/ejemplo/Principal.java
+++ src/ejemplo/Principal.java
@@ -3,6 +3,7 @@ package ejemplo;
 public class Principal{
    
     public static void main(String[] argumentos){
-       System.out.println("Hola a todos");
+       System.out.println("Saludos");
+       int num=5;
     }
 }
